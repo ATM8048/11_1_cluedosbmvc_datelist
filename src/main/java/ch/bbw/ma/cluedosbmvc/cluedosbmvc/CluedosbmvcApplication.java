2afed6b773b842688cf2db1f63f03ea9f7cec29d
Martin Atanasov <martin.atanasov@lernende.bbw.ch>
2023-08-29 12:20:30 +0200

    das Grundgerüst wurde generiert

---
+++ src/main/java/ch/bbw/ma/cluedosbmvc/cluedosbmvc/CluedosbmvcApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.ma.cluedosbmvc.cluedosbmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluedosbmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluedosbmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/ma/cluedosbmvc/cluedosbmvc/CluedosbmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.ma.cluedosbmvc.cluedosbmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CluedosbmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
